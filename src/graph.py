88dee4d550fa65c7068ff9a877736d37cfe07a9b
Eduardo Savian <104455792+eduardosavian@users.noreply.github.com>
2024-04-24 20:48:09 -0300

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -2,40 +2,54 @@ from queue import SimpleQueue
 
 class Graph:
     def __init__(self, directed = True):
-        self.node_map: list[str] = []
+        self.label_map: list[str] = []
         self.connections: list[list[int]] = []
         self.directed = directed
 
     def __getitem__(self, p: tuple[str, str]):
-        ai = self.node_map.index(p[0])
-        bi = self.node_map.index(p[1])
+        ai = self.label_map.index(p[0])
+        bi = self.label_map.index(p[1])
         return self.connections[ai][bi]
 
     def __setitem__(self, p: tuple[str, str], val: int):
-        ai = self.node_map.index(p[0])
-        bi = self.node_map.index(p[1])
+        ai = self.label_map.index(p[0])
+        bi = self.label_map.index(p[1])
         self.connections[ai][bi] = val
         if not self.directed:
             self.connections[bi][ai] = val
 
     def __repr__(self) -> str:
+        header = ''
         s = ''
+
+        col_widths = []
+        for l in self.label_map:
+            col_widths.append(len(l))
+
+        widest = max(col_widths)
+        s += ' ' * (widest + 1)
+        for i, label in enumerate(self.label_map):
+            s += label + ' ' * (widest - len(label) + 1)
+        s += '\n'
+
         for i, row in enumerate(self.connections):
-            s += f'{self.node_map[i]} | '
+            label = self.label_map[i]
+            s += label + ' ' * (widest - len(label) + 1)
             for conn in row:
-                s += f'{conn} '
+                pad = (widest - len(str(conn)) + 1) * ' '
+                s += f'{conn}{pad}'
             s += '\n'
         return s
 
     def add(self, label: str | list[str]):
-        if label in self.node_map:
+        if label in self.label_map or len(label) < 1:
             return
 
         if type(label) is list:
             for e in label:
                 self.add(e)
         else:
-            self.node_map.append(str(label))
+            self.label_map.append(str(label))
             new_row = [0 for _ in range(0, len(self.connections))]
             self.connections.append(new_row)
 
@@ -43,10 +57,10 @@ class Graph:
                 self.connections[i].append(0)
 
     def remove(self, label: str):
-        if label not in self.node_map:
+        if label not in self.label_map:
             return
 
-        idx = self.node_map.index(label)
+        idx = self.label_map.index(label)
         self.connections.pop(idx)
         for i, _ in enumerate(self.connections):
             self.connections[i].pop(idx)
@@ -56,7 +70,7 @@ class Graph:
         levels = [-1 for _ in range(0, len(self.connections))]
 
         queue = SimpleQueue()
-        start = self.node_map.index(starting_node)
+        start = self.label_map.index(starting_node)
 
         queue.put(start)
         levels[start] = 0
@@ -74,8 +88,6 @@ class Graph:
 
         res = []
         for i in range(0, len(self.connections)):
-            res.append((self.node_map[i], levels[i]))
+            res.append((self.label_map[i], levels[i]))
 
         return res
-
-
