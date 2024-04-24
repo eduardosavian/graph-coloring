cdb3dc44684ca6b72be27d6d6ef9d8825cafe284
Eduardo Savian <104455792+eduardosavian@users.noreply.github.com>
2024-04-24 20:49:47 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,18 +3,9 @@ from graph import Graph
 def main():
     g = Graph(directed=False)
 
-    g.add([
-        'RS',
-        'SC',
-        'PR',
-        'SP',
-    ])
-
-    g['SC', 'RS'] = 1
-    g['SC', 'PR'] = 1
-    g['PR', 'SP'] = 1
-    g['RS', 'SC'] = 1
-
+    g.add([ 'AAAA', 'BB', 'CCCC', 'D' ])
+    
+    
     print(g)
 
 if __name__ == "__main__":
